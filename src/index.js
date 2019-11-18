e6a14ac0f59a213dbb6b4ba0dbe9ff9092a616e4
Vladut Andrei <gvlad.piciorea@gmail.com>
2019-11-18 22:26:27 +0200

    Final

--- src/index.js
+++ src/index.js
@@ -14,14 +14,14 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const config = {
-    apiKey: "AIzaSyBP8hBnLJ8lN9MFb5cK7rTgoNa7NVjskuA",
-    authDomain: "game-site-d46c1.firebaseapp.com",
-    databaseURL: "https://game-site-d46c1.firebaseio.com",
-    projectId: "game-site-d46c1",
-    storageBucket: "game-site-d46c1.appspot.com",
-    messagingSenderId: "35595920511",
-    appId: "1:35595920511:web:ba7edd45c11cd183818b4a",
-    measurementId: "G-NBF9V390BH"
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: '',
 };
 firebase.initializeApp(config);
 firebase.firestore().settings({});
